be2c80c02c889e00326d2d2c8005a3ee2d380a4b
SSAISNEHITH <saisnehith4509@gmail.com>
2026-06-16 10:40:34 +0530

    New

---
+++ my-react/src/Animal.jsx
@@ -0,0 +1,20 @@
+import App from "./App.jsx"
+function animal(){
+return(
+    <>
+<h1>Animal Function</h1>  
+<div style={{display:"flex",gap:"20px"}}>
+    <p>
+        style={{textAlign:"justify"}}
+        Wild animals are species that live independently of humans,
+        surviving by finding their own food, shelter, and mates in nature. 
+        They include mammals, birds, reptiles, amphibians, fish, and insects, as well as uncultivated plants that grow in the wild 
+        Unlike domesticated animals, wild animals are not bred or cared for by humans, 
+        although some have adapted to urban environments, such as
+    </p>
+</div>
+<App></App>
+    </>
+);
+}
+export default animal;
--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,14 +1,23 @@
 function app(){
   return(
+    <>
+    <div>
+    <h1 style ={{color:"blue"}}>Bird</h1>
     <div style={{display:"flex"}}>
-    <h1 style={{color:"pink"}}>Bird</h1>
-    <img style={{width:250}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8lr-ly0AYLqze-rfRLvS5iRoQ7YWkM1uC_gK3BFqr8WZMY_afvxYq7gWi9W3WHscO3oHQEcs9RPyuQ_spmYUvV5TcHq-cGDAznLPLuxSagA&s=10"></img>
-    <p className="Birddetails">Conversation with Gemini
-give para about bird
-Birds are remarkable vertebrate animals characterized by their feathers, toothless beaked jaws, and the laying of hard-shelled eggs. They are endothermic, meaning they regulate their own body temperature, and possess a high metabolic rate, a four-chambered heart, and a strong, lightweight skeleton necessary for flight. While most birds are capable of flight—a key evolutionary trait that allows them to migrate, forage, and escape predators—some species, such as penguins and ostriches, have adapted to life on the ground or in the water. Beyond their biological classification, birds play essential roles in ecosystems across the globe, acting as pollinators, seed dispersers, and natural pest controllers, while also captivating humans with their complex songs, intricate nesting behaviors, and diverse, often vibrant, plumage.Sharing '
-my-react'Skip to the latest Gemini result
-</p>
+    <img style={{width:200}}
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCAOOQIqXkn4dFf_0mied78kTQpL62rX8mxg&s" alt="birds"/>
+    <p className="avian">Rooster. A rooster is a male chicken. ...
+        Crow. A crow is a black-colored bird. ...
+        Eagle. An eagle is a large and powerful bird. ...
+        Ostrich. An ostrich is the largest bird in the world. ...
+        Owl. An owl is a night bird. ...
+        Peacock. A peacock is one of the most beautiful birds. ...
+        Parrot. ...
+          Bat.
+    </p>
     </div>
+    </div>
+    </>
   );
 }
 export default app;
