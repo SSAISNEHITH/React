53debb7f92f30d5beb0663c16273d973b7ac89d4
SSAISNEHITH <saisnehith4509@gmail.com>
2026-06-16 09:59:04 +0530

    New

---
+++ my-react/src/App.jsx
@@ -0,0 +1,14 @@
+function app(){
+  return(
+    <div style={{display:"flex"}}>
+    <h1 style={{color:"pink"}}>Bird</h1>
+    <img style={{width:250}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8lr-ly0AYLqze-rfRLvS5iRoQ7YWkM1uC_gK3BFqr8WZMY_afvxYq7gWi9W3WHscO3oHQEcs9RPyuQ_spmYUvV5TcHq-cGDAznLPLuxSagA&s=10"></img>
+    <p className="Birddetails">Conversation with Gemini
+give para about bird
+Birds are remarkable vertebrate animals characterized by their feathers, toothless beaked jaws, and the laying of hard-shelled eggs. They are endothermic, meaning they regulate their own body temperature, and possess a high metabolic rate, a four-chambered heart, and a strong, lightweight skeleton necessary for flight. While most birds are capable of flight—a key evolutionary trait that allows them to migrate, forage, and escape predators—some species, such as penguins and ostriches, have adapted to life on the ground or in the water. Beyond their biological classification, birds play essential roles in ecosystems across the globe, acting as pollinators, seed dispersers, and natural pest controllers, while also captivating humans with their complex songs, intricate nesting behaviors, and diverse, often vibrant, plumage.Sharing '
+my-react'Skip to the latest Gemini result
+</p>
+    </div>
+  );
+}
+export default app;
